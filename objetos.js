349e4ffdfb78c330d6f5e8aaeddc66c9b9140d11
Matias Blanco <matias.david.blanco@gmail.com>
2022-04-12 22:30:42 -0300

    definición del constructor de un objeto

--- objetos.js
+++ objetos.js
@@ -11,10 +11,24 @@ let duck = {
 
   
   // creación de un método de un objeto
-  
+
   let duck = {
     name: "Aflac",
     numLegs: 2,
     sayName: function() {return "The name of this duck is " + duck.name + ".";}
   };
   duck.sayName();
+
+    // definición del constructor de un objeto
+
+  function Bird() {
+    this.name = "Albert";
+    this.color = "blue";
+    this.numLegs = 2;
+  }
+
+  function Bird(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 2;
+  }
