db64ff6591b0178025081984e080a23f69cc013b
Matias Blanco <matias.david.blanco@gmail.com>
2022-04-12 22:24:30 -0300

    creación de un método de un arreglo

--- objetos.js
+++ objetos.js
@@ -6,5 +6,15 @@ let duck = {
   };
 
   // acceso a propiedades de un objeto
-  
+
   console.log(duck.name);
+
+  
+  // creación de un método de un objeto
+  
+  let duck = {
+    name: "Aflac",
+    numLegs: 2,
+    sayName: function() {return "The name of this duck is " + duck.name + ".";}
+  };
+  duck.sayName();
