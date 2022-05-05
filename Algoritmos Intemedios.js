08d77ba42ff27b9755802b7975fda4900b1b61c6
Matias Blanco <matias.david.blanco@gmail.com>
2022-05-05 15:37:31 -0300

    función periodo orbital

--- Algoritmos Intemedios.js
+++ Algoritmos Intemedios.js
@@ -69,3 +69,27 @@ console.log(bob.setFirstName("Haskell"));
 console.log(bob.getFullName());
 console.log(bob.setFullName("Haskell Curry"));
 console.log(bob.getFullName());
+
+  
+// función que calcula el periodo orbital de un objeto alrededor de la tierra. 
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const newArr = [];
+
+  //Looping through each key in arr object
+  for (let elem in arr) {
+    //Rounding off the orbital period value
+    const orbitalPer = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(arr[elem].avgAlt + earthRadius, 3) / GM)
+    );
+    //Adding new object with orbitalPeriod property
+    newArr.push({name: arr[elem].name, orbitalPeriod: orbitalPer});
+  }
+
+  return newArr;
+}
+
+
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873}]);
